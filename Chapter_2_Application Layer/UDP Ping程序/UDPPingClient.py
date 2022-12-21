6f486fefef4c5ee544aa0d2da2bd02062f6e607e
Yang Li <58134113+YangLi0306@users.noreply.github.com>
2022-12-21 10:53:05 +0800

    Update UDPPingClient.py

--- Chapter_2_Application Layer/UDP Ping程序/UDPPingClient.py
+++ Chapter_2_Application Layer/UDP Ping程序/UDPPingClient.py
@@ -1,1 +1,29 @@
+# -*- coding = utf-8 -*-
+# @Time: 2022/12/21 10:08
+# @Author: Li Yang
+# @File: UDPPingClient.py
+# @Software: PyCharm
+
+import time
+from socket import *
+
+serverName = '127.0.0.1'
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+# 设置最多等待时间1s
+clientSocket.settimeout(1)
+
+for i in range(1,11):
+    sendTime = time.time()
+    message = 'Ping ' + str(i) + ' '+time.asctime()
+    # 发送Ping命令，并打印
+    clientSocket.sendto(message.encode(), (serverName, serverPort))
+    print('>' + message)
+    try:
+        modifiedMessage, serverAddress = clientSocket.recvfrom(1024)
+        RTT = time.time() - sendTime
+        print("%s,  RTT = %.3fs" % (modifiedMessage.decode(), RTT))
+    except Exception as e:
+        print("Request timed out")
 
